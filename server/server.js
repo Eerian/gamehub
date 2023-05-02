e0935605bf1f8afa39063c0096ad1cae472c8e55
Erion <eerian00@gmail.com>
2023-05-02 13:19:04 -0400

    adds user Login/Signup functionality using jsonwebtoken

--- server/server.js
+++ server/server.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const userRouter = require('./routes/user.jsx')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/user.js");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 //Cors
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
   })
-)
+);
 
 //middleware
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/user', userRouter)
+app.use("/api/user", userRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('Listening on port 5000')
-    })
+      console.log("Listening on port 5000");
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
