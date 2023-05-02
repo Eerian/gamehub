ddc11d94d21a8d54513c7007e3d3312e44d26abf
Erion <eerian00@gmail.com>
2023-05-02 14:00:58 -0400

    added User login/signup forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Header from './components/Header'
-import Games from './components/Games'
-import Cart from './components/Cart'
-import Footer from './components/Footer'
-import GameDetails from './components/GameDetails'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Games from "./components/Games";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import GameDetails from "./components/GameDetails";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -16,11 +18,13 @@ function App() {
           <Route exact path="/" element={<Games />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/game/:gameId" element={<GameDetails />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
         <Footer />
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
 import { ContextProvider } from "./Context";
-import { AuthContextProvider } from "./context/AuthContext.js";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
