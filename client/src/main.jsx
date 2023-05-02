04a96a9bd662fb2b0076a05558c2530c656955a5
Erion <eerian00@gmail.com>
2023-05-02 13:36:07 -0400

    creates auth context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './main.css'
-import { ContextProvider } from './Context'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./main.css";
+import { ContextProvider } from "./Context";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthContextProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </AuthContextProvider>
+);
