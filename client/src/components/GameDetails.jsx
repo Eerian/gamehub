554be8076c11f2546407e4468c440885a2ebe195
Erion <eerian00@gmail.com>
2023-05-08 11:10:21 -0400

    changed some styling

--- client/src/components/GameDetails.jsx
+++ client/src/components/GameDetails.jsx
@@ -1,43 +1,43 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function GameDetails() {
   // const { allGames, addToCart } = useContext(Context)
-  const { gameId } = useParams()
-  const [game, setGame] = useState()
-  const [reviews, setReviews] = useState([])
-  const [loading, setLoading] = useState(true)
+  const { gameId } = useParams();
+  const [game, setGame] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   async function fetchData() {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': 'acfe886c87msheed5cea628f045ap135c40jsnd20aecb2c777',
-        'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
+        "X-RapidAPI-Key": "acfe886c87msheed5cea628f045ap135c40jsnd20aecb2c777",
+        "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
       },
-    }
+    };
     try {
       const response = await fetch(
         `https://rawg-video-games-database.p.rapidapi.com/games/${gameId}?key=3b9dae53a7564a4b938725f718d064da`,
         options
-      )
-      if (!response.ok) throw await response.json()
-      const selectedGame = await response.json()
-      setGame(selectedGame)
-      console.log(selectedGame)
-      setLoading(false)
+      );
+      if (!response.ok) throw await response.json();
+      const selectedGame = await response.json();
+      setGame(selectedGame);
+      console.log(selectedGame);
+      setLoading(false);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   if (loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
+
   return (
     <div className="game-details-page">
       <div>
@@ -55,7 +55,7 @@ function GameDetails() {
         <h3>Reviews:</h3>
       </div>
     </div>
-  )
+  );
 }
 
-export default GameDetails
+export default GameDetails;
